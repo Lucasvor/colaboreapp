b9def642a2ba9dc2499e3e70590172ffd2036529
Phelipe <phelipeptozzi@hotmail.com>
2020-11-17 01:17:12 -0300

    Definição layout home ong

--- lib/Screens/HomeOng/HomeOngForm.dart
+++ lib/Screens/HomeOng/HomeOngForm.dart
@@ -1,4 +1,5 @@
-import 'package:colaboreapp/Screens/HomeOng/HomeOng.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:colaboreapp/Model/ong.dart';
 import 'package:colaboreapp/bloc/HomeOng/homeong_bloc.dart';
 import 'package:colaboreapp/bloc/auth/auth_bloc.dart';
 import 'package:colaboreapp/components/rounded_button.dart';
@@ -7,6 +8,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../bloc/Home/home_bloc.dart';
+import '../../constants.dart';
 
 class HomeOngForm extends StatefulWidget {
   final UserRepository userRepository;
@@ -34,42 +39,170 @@ class _HomeOngFormState extends State<HomeOngForm> {
       child: BlocBuilder<HomeongBloc, HomeongState>(
         builder: (context, state) {
           return Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Center(
-                  child: Text('Bem vindo Ong: ${widget.usuario.displayName}'),
-                ),
-                Container(
-                  width: size.width * 0.3,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: RoundedButton(
-                      color: Colors.red,
-                      text: 'Sair',
-                      textColor: Colors.white,
-                      press: () async {
-                        try {
-                          //blalala
-                          //asdasdasda
-                          await widget.userRepository.singOut();
-                          BlocProvider.of<AuthBloc>(context).add(
-                            AuthSplash(),
-                          );
-                          Navigator.of(context).pop();
-                        } catch (e) {
-                          print(e);
-                          print(e);
-                        }
-                      },
+            width: double.infinity,
+            height: size.height,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 20),
+              child: Column(
+                children: <Widget>[
+                  RichText(
+                    text: TextSpan(
+                      text: 'Nome da ONG\n',
+                      style: TextStyle(fontSize: 20, color: Colors.black),
+                      children: <TextSpan>[
+                        // TextSpan(
+                        //   text: '  ' + nameShort,
+                        //   //'${widget.usuario.displayName}',
+                        //   style: TextStyle(
+                        //       fontWeight: FontWeight.bold, fontSize: 35),
+                        // ),
+                      ],
                     ),
                   ),
-                ),
-              ],
+                  Expanded(
+                    child: Container(
+                      height: size.height * 0.2,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Expanded(
+                            child: Container(
+                              height: size.height * 0.2,
+                              margin: EdgeInsets.all(20),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: FlatButton(
+                                  onPressed: () {},
+                                  color: kPrimaryColorGreen,
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: <Widget>[
+                                      SvgPicture.asset(
+                                        "assets/images/schedule.svg",
+                                        height: size.height * 0.1,
+                                      ),
+                                      Text(
+                                        'Imagem da ong',
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: size.height * 0.2,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Expanded(
+                          child: Container(
+                            height: size.height * 0.2,
+                            margin: EdgeInsets.all(20),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: FlatButton(
+                                onPressed: () {},
+                                color: kPrimaryColorGreen,
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: <Widget>[
+                                    SvgPicture.asset(
+                                      "assets/images/schedule.svg",
+                                      height: size.height * 0.1,
+                                    ),
+                                    Text(
+                                      'Eventos',
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            height: size.height * 0.2,
+                            margin: EdgeInsets.all(20),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: FlatButton(
+                                onPressed: () {},
+                                color: kPrimaryColorGreen,
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: <Widget>[
+                                    SvgPicture.asset(
+                                      "assets/images/donation.svg",
+                                      height: size.height * 0.1,
+                                    ),
+                                    Text(
+                                      'Doações captadas',
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: size.width * 0.3,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: RoundedButton(
+                        color: Colors.red,
+                        text: 'Sair',
+                        textColor: Colors.white,
+                        press: () async {
+                          try {
+                            await widget.userRepository.singOut();
+                            BlocProvider.of<AuthBloc>(context).add(
+                              AuthSplash(),
+                            );
+                            Navigator.of(context).pop();
+                          } catch (e) {
+                            print(e);
+                            print(e);
+                          }
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           );
         },
+      ),
+    );
+  }
+
+  Widget _buildCircleAvatar(String url, String nome) {
+    return Hero(
+      tag: nome,
+      child: ClipOval(
+        child: AspectRatio(
+          aspectRatio: 1,
+          child: CachedNetworkImage(
+            imageUrl: url,
+            placeholder: (context, url) => new CircularProgressIndicator(),
+            errorWidget: (context, url, error) => new Icon(Icons.error),
+            fit: BoxFit.scaleDown,
+          ),
+        ),
       ),
     );
   }
