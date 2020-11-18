6f43f31e25b933c50e82ac03eaba63d84dab28c9
Lucas <lucsvor@gmail.com>
2020-11-17 22:31:28 -0300

    att

--- lib/Screens/Eventos/criaevento.dart
+++ lib/Screens/Eventos/criaevento.dart
@@ -123,11 +123,13 @@ class _CriaEventoFormState extends State<CriaEventoForm> {
                   text: "CRIAR EVENTO",
                   press: () async {
                     try {
-                      if (descricaoController.text.length > 10) {
+                      if (descricaoController.text.length > 10 &&
+                          tituloController.text.length > 5) {
                         var firestoreOng = FirestoreOngs();
                         firestoreOng.makeEvento(new Evento(
                             ong: widget.ong.nome,
                             data: new DateTime.now(),
+                            titulo: tituloController.text,
                             mensagem: descricaoController.text));
                         Navigator.of(context).pop();
                       } else {
